f42411370891e6d29b6db646795339bf993eeaac
bildgaul <bildgaul@EPSG1-825021>
2021-09-14 19:24:00 -0500

    Add project files.

---
+++ Bears/IBear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bears
+{
+    interface IBear
+    {
+        void Maul();
+
+        void Hibernate();
+    }
+}
